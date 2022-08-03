08cda17eb1368d199328afa095721a19c716856c
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2022-08-03 15:28:30 +0200

    added reusable-resource timeline extractor..

--- src/rr_timeline_extractor.cpp
+++ src/rr_timeline_extractor.cpp
@@ -1,35 +1,38 @@
-#include "sv_timeline_extractor.h"
+#include "rr_timeline_extractor.h"
 #include "gui_server.h"
-#include "state_variable.h"
+#include "reusable_resource.h"
 
 namespace ratio::gui
 {
-    sv_timeline_extractor::sv_timeline_extractor(gui_server &gs) : timeline_extractor(gs, gs.get_solver().get_type(STATE_VARIABLE_NAME)) {}
+    rr_timeline_extractor::rr_timeline_extractor(gui_server &gs) : timeline_extractor(gs, gs.get_solver().get_type(REUSABLE_RESOURCE_NAME)) {}
 
-    std::vector<crow::json::wvalue> sv_timeline_extractor::extract_timelines() const noexcept
+    std::vector<crow::json::wvalue> rr_timeline_extractor::extract_timelines() const noexcept
     {
         std::vector<crow::json::wvalue> tls;
-        // we partition atoms for each state-variable they might insist on..
-        std::unordered_map<const ratio::core::item *, std::vector<ratio::core::atom *>> sv_instances;
-        for (const auto &atm : static_cast<const ratio::solver::state_variable &>(tp).get_atoms())
+        // we partition atoms for each reusable-resource they might insist on..
+        std::unordered_map<ratio::core::item *, std::vector<ratio::core::atom *>> rr_instances;
+        for (const auto &atm : static_cast<const ratio::solver::reusable_resource &>(tp).get_atoms())
             if (gs.get_solver().get_sat_core()->value(get_sigma(gs.get_solver(), *atm)) == semitone::True) // we filter out those which are not strictly active..
             {
                 const auto c_scope = atm->get(TAU_KW);
                 if (const auto enum_scope = dynamic_cast<ratio::core::enum_item *>(&*c_scope))
                     for (const auto &val : gs.get_solver().get_ov_theory().value(enum_scope->get_var()))
-                        sv_instances[static_cast<const ratio::core::item *>(val)].emplace_back(atm);
+                        rr_instances[static_cast<ratio::core::item *>(val)].emplace_back(atm);
                 else
-                    sv_instances[static_cast<ratio::core::item *>(&*c_scope)].emplace_back(atm);
+                    rr_instances[static_cast<ratio::core::item *>(&*c_scope)].emplace_back(atm);
             }
 
-        for (const auto &[sv, atms] : sv_instances)
+        for (const auto &[rr, atms] : rr_instances)
         {
             crow::json::wvalue tl;
-            tl["id"] = get_id(*sv);
+            tl["id"] = get_id(*rr);
 #ifdef COMPUTE_NAMES
-            tl["name"] = gs.get_solver().guess_name(*sv);
+            tl["name"] = gs.get_solver().guess_name(*rr);
 #endif
-            tl["type"] = STATE_VARIABLE_NAME;
+            tl["type"] = REUSABLE_RESOURCE_NAME;
+
+            const auto c_capacity = gs.get_solver().ratio::core::core::arith_value(static_cast<ratio::core::complex_item *>(rr)->get(REUSABLE_RESOURCE_CAPACITY));
+            tl["capacity"] = to_json(c_capacity);
 
             // for each pulse, the atoms starting at that pulse..
             std::map<semitone::inf_rational, std::set<ratio::core::atom *>> starting_atoms;
@@ -66,9 +69,14 @@ namespace ratio::gui
                 j_val["to"] = to_json(*p);
 
                 std::vector<uintptr_t> j_atms;
+                semitone::inf_rational c_usage; // the concurrent resource usage..
                 for (const auto &atm : overlapping_atoms)
+                {
+                    c_usage += gs.get_solver().ratio::core::core::arith_value(atm->get(REUSABLE_RESOURCE_USE_AMOUNT_NAME));
                     j_atms.emplace_back(get_id(*atm));
+                }
                 j_val["atoms"] = std::move(j_atms);
+                j_val["usage"] = to_json(c_usage);
                 j_vals.emplace_back(std::move(j_val));
 
                 if (const auto at_start_p = starting_atoms.find(*p); at_start_p != starting_atoms.cend())
