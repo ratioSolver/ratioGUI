e4c519dde1f31c8040e167693047c7ffa8074723
riccardodebenedictis <riccardo.debenedictis@istc.cnr.it>
2023-03-26 16:11:20 +0200

    minor cleanings..

--- static/solverD3.js
+++ static/solverD3.js
@@ -1,26 +1,6 @@
 import { Solver } from "./solver.js";
 
-import { bisector, max, range } from 'd3-array'
-import { axisBottom } from 'd3-axis'
-import { drag } from 'd3-drag'
-import { forceCenter, forceLink, forceManyBody, forceSimulation, forceX } from 'd3-force'
-import { scaleBand, scaleLinear, scaleOrdinal, scaleSequential } from 'd3-scale'
-import { interpolateRdYlGn } from 'd3-scale-chromatic'
-import { select, selectAll } from 'd3-selection'
-import { area } from 'd3-shape'
-import { zoom } from 'd3-zoom'
-
-const d3 = {
-    bisector, max, range,
-    axisBottom,
-    drag,
-    forceCenter, forceLink, forceManyBody, forceSimulation, forceX,
-    interpolateRdYlGn,
-    scaleBand, scaleLinear, scaleOrdinal, scaleSequential,
-    select, selectAll,
-    area,
-    zoom
-}
+import * as d3 from 'd3';
 
 const color_interpolator = d3.scaleSequential(d3.interpolateRdYlGn).domain([15, 0]);
 const bisect_value = d3.bisector(d => d.to).left;
