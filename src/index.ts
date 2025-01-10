4ded791864719bb25fbcbb17d36170d7a4b8731a
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2025-01-10 16:42:27 +0100

    bump version to 0.0.8 in package.json and package-lock.json; integrate Bootstrap and export additional modules in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 import cytoscape from 'cytoscape';
 import dagre from 'cytoscape-dagre';
 import cytoscapePopper, { RefElement } from 'cytoscape-popper';
 import tippy from 'tippy.js';
 
+export * from './app';
 export { solver } from './solver';
 export { SolverGraph } from './solver_graph';
 export { TimelinesChart } from './solver_timelines';
+export * from './solver_components';
 
 function tippyFactory(ref: RefElement, content: HTMLElement) {
   // Since tippy constructor requires DOM element/elements, create a placeholder
