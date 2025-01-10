c23dcf1a5c5815f1c2678decb9069818ac937632
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2025-01-10 18:19:19 +0100

    bump version to 0.0.13 in package.json; switch from plotly.js to plotly.js-dist-min for improved performance

--- src/solver_timelines.ts
+++ src/solver_timelines.ts
@@ -1,6 +1,6 @@
 import { Component } from "./app";
 import { solver } from "./solver";
-import Plotly, { Shape } from 'plotly.js';
+import Plotly, { Shape } from 'plotly.js-dist-min';
 export class TimelinesChart extends Component<solver.Solver, HTMLDivElement> implements solver.SolverListener {
 
   private current_time: Partial<Shape> = {
